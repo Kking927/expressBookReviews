60d176a2ad40522057f14da692d9dbeb71af80d8
Kking927 <kking927@hotmail.com>
2025-08-01 13:41:02 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,29 +1,72 @@
-const express = require("express");
-const fs = require("fs");
-const router = express.Router();
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
 
-router.post("/register", (req, res) => {
-  const { username, password } = req.body;
-
-  if (!username || !password) {
-    return res.status(400).json({ message: "Username and password are required" });
-  }
-
-  let users = [];
-  if (fs.existsSync("users.json")) {
-    const data = fs.readFileSync("users.json", "utf8");
-    users = JSON.parse(data);
-  }
-
-  const existingUser = users.find((user) => user.username === username);
-  if (existingUser) {
-    return res.status(409).json({ message: "User already exists!" });
-  }
-
-  users.push({ username, password });
-
-  fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
-  return res.status(201).json({ message: "Customer successfully registered. Now you can login" });
+public_users.post("/register", (req, res) => {
+  // registration is handled in auth_users.js - no code here needed
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
-module.exports = router;
+public_users.get('/', function (req, res) {
+  return res.status(200).send(JSON.stringify(books, null, 4));
+});
+
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
+public_users.get('/author/:author', function (req, res) {
+  const authorName = req.params.author.toLowerCase();
+  const booksByAuthor = [];
+
+  Object.keys(books).forEach(key => {
+    if (books[key].author.toLowerCase() === authorName) {
+      booksByAuthor.push(books[key]);
+    }
+  });
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: "No books found by that author" });
+  }
+});
+
+public_users.get('/title/:title', function (req, res) {
+  const requestedTitle = req.params.title.toLowerCase();
+  const booksByTitle = [];
+
+  Object.keys(books).forEach(key => {
+    if (books[key].title.toLowerCase() === requestedTitle) {
+      booksByTitle.push(books[key]);
+    }
+  });
+
+  if (booksByTitle.length > 0) {
+    return res.status(200).json(booksByTitle);
+  } else {
+    return res.status(404).json({ message: "No books found with that title" });
+  }
+});
+
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    return res.status(200).json(book.reviews);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
+module.exports.general = public_users;
