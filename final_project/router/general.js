30cc0e3377daabe6a33744b07412553282be2ce0
Kking927 <kking927@hotmail.com>
2025-08-01 14:45:32 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,72 +1,30 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
+let public_users = express.Router();
 
-public_users.post("/register", (req, res) => {
-  // registration is handled in auth_users.js - no code here needed
-  return res.status(300).json({ message: "Yet to be implemented" });
+// Get all books
+public_users.get("/", (req, res) => {
+  res.send(JSON.stringify(books, null, 4));
 });
 
-public_users.get('/', function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
-});
-
-public_users.get('/isbn/:isbn', function (req, res) {
+// Get book by ISBN
+public_users.get("/isbn/:isbn", (req, res) => {
   const isbn = req.params.isbn;
-  const book = books[isbn];
-
-  if (book) {
-    return res.status(200).json(book);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
-  }
+  res.send(books[isbn]);
 });
 
-public_users.get('/author/:author', function (req, res) {
-  const authorName = req.params.author.toLowerCase();
-  const booksByAuthor = [];
-
-  Object.keys(books).forEach(key => {
-    if (books[key].author.toLowerCase() === authorName) {
-      booksByAuthor.push(books[key]);
-    }
-  });
-
-  if (booksByAuthor.length > 0) {
-    return res.status(200).json(booksByAuthor);
-  } else {
-    return res.status(404).json({ message: "No books found by that author" });
-  }
+// Get books by author
+public_users.get("/author/:author", (req, res) => {
+  const author = req.params.author;
+  const result = Object.values(books).filter(book => book.author === author);
+  res.send(result);
 });
 
-public_users.get('/title/:title', function (req, res) {
-  const requestedTitle = req.params.title.toLowerCase();
-  const booksByTitle = [];
-
-  Object.keys(books).forEach(key => {
-    if (books[key].title.toLowerCase() === requestedTitle) {
-      booksByTitle.push(books[key]);
-    }
-  });
-
-  if (booksByTitle.length > 0) {
-    return res.status(200).json(booksByTitle);
-  } else {
-    return res.status(404).json({ message: "No books found with that title" });
-  }
-});
-
-public_users.get('/review/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-
-  if (book) {
-    return res.status(200).json(book.reviews);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
-  }
+// Get books by title
+public_users.get("/title/:title", (req, res) => {
+  const title = req.params.title;
+  const result = Object.values(books).filter(book => book.title === title);
+  res.send(result);
 });
 
 module.exports.general = public_users;
