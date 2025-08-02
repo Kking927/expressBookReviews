88e1771574f33a6ff6aa63552e693b6e41545c27
Kking927 <kking927@hotmail.com>
2025-08-02 15:15:50 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,37 +21,57 @@ public_users.post("/register", (req, res) => {
     }
   });  
 
-// Get all books
-public_users.get('/', (req, res) => {
-  res.send(JSON.stringify(books, null, 4));
+// Get the book list available in the shop
+public_users.get("/", async function (req, res) {
+    try {
+        const data = await promiseCb((resolve) => {
+            const booksList = Object.values(books);
+            resolve(booksList);
+    }, 3000);
+
+    return res.status(200).json(data);
+} catch (error) {
+    return res.status(500).json({ message: "Internal server error."});
+}
 });
 
-// Get book details by ISBN
-public_users.get('/isbn/:isbn', (req, res) => {
-  const isbn = req.params.isbn;
-  if (books[isbn]) {
-    res.send(books[isbn]);
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
-});
-
-// Get book details by author
-public_users.get('/author/:author', (req, res) => {
-  const author = req.params.author.toLowerCase();
-  let filteredBooks = {};
-
-  for (const [isbn, book] of Object.entries(books)) {
-    if (book.author.toLowerCase() === author) {
-      filteredBooks[isbn] = book;
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+        const data = await promiseCb((resolve) => {
+        const isbn = req.params.isbn + "";
+        const book = books[isbn];
+        resolve(book);
+        }, 3000);
+        if (data) {
+            return res.status(200).json(data);
+        }     
+        return res.status(404).json({message: "Invalid ISBN"});
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error"});
     }
-  }
+});
 
-  if (Object.keys(filteredBooks).length > 0) {
-    res.send(JSON.stringify(filteredBooks, null, 4));
-  } else {
-    res.status(404).json({ message: "No books found for the given author" });
-  }
+// Get book details based on author
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const data = await promiseCb((resolve) => {
+            const author = (req.params.author + "").toLowerCase();
+            const booksList = Object.values(books);
+            const newBooks = booksList.filter((book) => {
+                return book.author.toLowerCase().includes(author);
+            });
+            resolve(newBooks);
+        }, 3000);
+
+        if (Array.isArray(data) && data.length) {
+            return res.status(200).json(data);
+        }
+
+        return res.status(404).json({ message: "Invalid author." });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    } 
 });
 
 // Get book details by title
